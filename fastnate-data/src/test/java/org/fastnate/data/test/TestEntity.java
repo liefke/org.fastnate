55a5e04ce73416df4acef31ef60dc7ed0974182f
Tobias Liefke <git@liefke.biz>
2016-06-02 22:43:28 +0200

    Excplicitly set identity strategy, as sequence is default now

--- fastnate-data/src/test/java/org/fastnate/data/test/TestEntity.java
+++ fastnate-data/src/test/java/org/fastnate/data/test/TestEntity.java
@@ -3,6 +3,7 @@ package org.fastnate.data.test;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
@@ -22,7 +23,7 @@ import lombok.Setter;
 public class TestEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ManyToOne
