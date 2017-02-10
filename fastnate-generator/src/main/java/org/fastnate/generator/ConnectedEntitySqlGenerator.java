3175d88df3dc05ea4c87f5c76633f55faafb28fb
Tobias Liefke <git@liefke.biz>
2017-02-10 08:27:49 +0100

    Log count of statements executed every minute

--- fastnate-generator/src/main/java/org/fastnate/generator/ConnectedEntitySqlGenerator.java
+++ fastnate-generator/src/main/java/org/fastnate/generator/ConnectedEntitySqlGenerator.java
@@ -9,18 +9,52 @@ import org.fastnate.generator.context.GeneratorContext;
 import org.fastnate.generator.statements.EntityStatement;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
- * An extenson of an {@link EntitySqlGenerator} which writes the SQL statements directly to a database connection.
+ * An extension of an {@link EntitySqlGenerator} which writes the SQL statements directly to a database connection.
  *
  * @author Tobias Liefke
  */
+@Slf4j
 public class ConnectedEntitySqlGenerator extends EntitySqlGenerator {
 
-	private final Statement statement;
+	/** The count of milliseconds to wait, until a log message with the current count of statements is written. */
+	private static final long MILLISECONDS_BETWEEN_LOG_MESSAGES = 60 * 1000;
 
+	private static String trimStatement(final String sql) {
+		int start = 0;
+		int end = sql.length();
+		while (start < end) {
+			final char c = sql.charAt(start);
+			if (c != ';' && !Character.isWhitespace(c)) {
+				break;
+			}
+			start++;
+		}
+		while (start < end) {
+			final char c = sql.charAt(end - 1);
+			if (c != ';' && !Character.isWhitespace(c)) {
+				break;
+			}
+			end--;
+		}
+		return sql.substring(start, end);
+	}
+
+	/** The database connection that was used when creating this generator. */
 	@Getter
 	private final Connection connection;
+
+	/** Used to execute all SQL statements. */
+	private final Statement statement;
+
+	/** The last time that we have written a log message about the count . */
+	private long lastLogTime;
+
+	/** The count of statements that we executed up to now. */
+	@Getter
+	private long statementsCount;
 
 	/**
 	 * Creates a new generator that writes to a database connection.
@@ -53,6 +87,7 @@ public class ConnectedEntitySqlGenerator extends EntitySqlGenerator {
 
 	@Override
 	public void close() throws IOException {
+		log.info("{} SQL statements successfully executed", this.statementsCount);
 		super.close();
 		try {
 			this.statement.close();
@@ -73,14 +108,19 @@ public class ConnectedEntitySqlGenerator extends EntitySqlGenerator {
 
 	@Override
 	public void writeStatement(final EntityStatement stmt) throws IOException {
-		String sql = getContext().getDialect().createSql(stmt);
-		if (sql.endsWith(";\n")) {
-			sql = sql.substring(0, sql.length() - 2);
-		}
+		final String sql = trimStatement(getContext().getDialect().createSql(stmt));
 		try {
 			this.statement.executeUpdate(sql);
 		} catch (final SQLException e) {
 			throw new IOException("Could not execute statement: " + sql, e);
+		}
+		this.statementsCount++;
+		final long currentTime = System.currentTimeMillis();
+		if (currentTime - this.lastLogTime >= MILLISECONDS_BETWEEN_LOG_MESSAGES) {
+			this.lastLogTime = currentTime;
+			if (this.statementsCount > 1) {
+				log.info("{} SQL statements executed", this.statementsCount);
+			}
 		}
 	}
 
